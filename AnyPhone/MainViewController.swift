16081bf264e7489311193c6037b0ec61e172eedf
Benjamin Harvey <bharv410@users.noreply.github.com>
2016-02-02 19:25:26 -0800

    bam

--- AnyPhone/MainViewController.swift
+++ AnyPhone/MainViewController.swift
@@ -49,6 +49,7 @@ class MainViewController: UIViewController {
       dismissViewControllerAnimated(true, completion: nil)
     }
   }
+
   @IBAction func backClick(sender: UIButton) {
     self.dismissViewControllerAnimated(true) { () -> Void in
       
@@ -123,6 +124,15 @@ class MainViewController: UIViewController {
       return value
     }
     return false
+  }
+  
+  func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+    return UIColor(
+      red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+      green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+      blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+      alpha: CGFloat(1.0)
+    )
   }
 
   @IBAction func clickedPrivacy(sender: UIButton) {
